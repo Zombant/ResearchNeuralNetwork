355fbde7021052da73d0ee61bb66fea8b067862d
Anthony Piccinone <anthony.piccinone@gmail.com>
2019-01-08 22:02:26 -0500

    Added feed-forward method

--- src/network/Network.java
+++ src/network/Network.java
@@ -1,5 +1,7 @@
 package network;
 
+import java.util.Arrays;
+
 public class Network {
 
     //NEURONS ARE STORED AS ARRAYS
@@ -47,12 +49,44 @@ public class Network {
         ////////////////////////////
     }
 
+    //Feed Forward method
+    public double[] calculate(double... input){
+        //Make sure that the input parameter matches the number of input neurons
+        if(input.length != this.INPUT_SIZE){ return null; }
+
+        //Output of first layer is the same as the input
+        this.output[0] = input;
+
+        //for every layer, starting at 1 (because first layer is just the input
+        for(int layer = 1; layer < NETWORK_SIZE; layer++){
+            for(int neuron = 0; neuron < NETWORK_LAYER_SIZES[layer]; neuron++){
+
+                //Get the sum and enter it into the activation function
+                double sum = 0;
+                for(int prevNeuron = 0; prevNeuron < NETWORK_LAYER_SIZES[layer - 1]; prevNeuron++){
+                    sum += output[layer - 1][prevNeuron] * weights[layer][neuron][prevNeuron];
+                }
+                sum += biases[layer][neuron];
+                output[layer][neuron] = sigmoid(sum);
+
+            }
+        }
+        return output[NETWORK_SIZE - 1];
+
+    }
+
+    private double sigmoid(double x){
+        return 1d / (1 + Math.exp(-x));
+    }
+
 
     public static void main(String[] args){
         /*
         Create a network
             new Network(4,2,3,1) will have 4 neurons in first layer, etc.
          */
-        Network network = new Network(1,2,3,4);
+        Network network = new Network(4,2,3,4);
+        double[] output = network.calculate(0.2, 0.9, 0.3, 0.4);
+        System.out.println(Arrays.toString(output));
     }
 }
