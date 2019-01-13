c9a12d0ce1bfc36ab5a6e6107a782d7366d2c82c
Anthony Piccinone <anthony.piccinone@gmail.com>
2019-01-13 12:45:48 -0500

    Refactor variable names

--- src/network/Network.java
+++ src/network/Network.java
@@ -6,54 +6,54 @@ public class Network {
 
     //NEURONS ARE STORED AS ARRAYS
     //first index is layer, second is neuron
-    private double[][] output;
+    private double[][] neuronOutputs;
     //first index is layer, second is neuron, third is neuron from previous layer
-    private double[][][] weights;
+    private double[][][] neuronWeights;
     //first index is layer, second is neuron
-    private double[][] biases;
+    private double[][] neuronBiases;
 
     //TODO: Make comment
-    private double[][] error_signal;
-    private double[][] output_derivative;
+    private double[][] errorSignal;
+    private double[][] outputDerivative;
 
-    public final int[] NETWORK_LAYER_SIZES; //how many neurons on each layer
+    public final int[] NUMBER_OF_NEURONS_IN_LAYER; //how many neurons on each layer
 
-    public final int INPUT_SIZE; //number of neurons in input layer
-    public final int OUTPUT_SIZE; //number of neurons in output layer
-    public final int NETWORK_SIZE; //number of layers
+    public final int INPUT_LAYER_SIZE; //number of neurons in input layer
+    public final int OUTPUT_LAYER_SIZE; //number of neurons in neuronOutputs layer
+    public final int NUMBER_OF_LAYERS; //number of layers
 
-    public Network(int... NETWORK_LAYER_SIZES) {
-        this.NETWORK_LAYER_SIZES = NETWORK_LAYER_SIZES;
-        this.INPUT_SIZE = NETWORK_LAYER_SIZES[0];
-        this.NETWORK_SIZE = NETWORK_LAYER_SIZES.length;
-        this.OUTPUT_SIZE = NETWORK_LAYER_SIZES[NETWORK_SIZE - 1];
+    public Network(int... NUMBER_OF_NEURONS_IN_LAYER) {
+        this.NUMBER_OF_NEURONS_IN_LAYER = NUMBER_OF_NEURONS_IN_LAYER;
+        this.INPUT_LAYER_SIZE = NUMBER_OF_NEURONS_IN_LAYER[0];
+        this.NUMBER_OF_LAYERS = NUMBER_OF_NEURONS_IN_LAYER.length;
+        this.OUTPUT_LAYER_SIZE = NUMBER_OF_NEURONS_IN_LAYER[NUMBER_OF_LAYERS - 1];
 
 
         ///////SET UP NEURONS///////
 
         //Set up using number of layers but not the size
-        this.output = new double[NETWORK_SIZE][];
-        this.weights = new double[NETWORK_SIZE][][];
-        this.biases = new double[NETWORK_SIZE][];
+        this.neuronOutputs = new double[NUMBER_OF_LAYERS][];
+        this.neuronWeights = new double[NUMBER_OF_LAYERS][][];
+        this.neuronBiases = new double[NUMBER_OF_LAYERS][];
 
         //TODO: Make comment
-        this.error_signal = new double[NETWORK_SIZE][];
-        this.output_derivative = new double[NETWORK_SIZE][];
+        this.errorSignal = new double[NUMBER_OF_LAYERS][];
+        this.outputDerivative = new double[NUMBER_OF_LAYERS][];
 
         //Set the size of each layer
         //iterate through each layer
-        for(int i = 0; i < NETWORK_SIZE; i++){
-            this.output[i] = new double[NETWORK_LAYER_SIZES[i]];
+        for(int i = 0; i < NUMBER_OF_LAYERS; i++){
+            this.neuronOutputs[i] = new double[NUMBER_OF_NEURONS_IN_LAYER[i]];
             //TODO: Make comment
-            this.error_signal[i] = new double[NETWORK_LAYER_SIZES[i]];
-            this.output_derivative[i] = new double[NETWORK_LAYER_SIZES[i]];
+            this.errorSignal[i] = new double[NUMBER_OF_NEURONS_IN_LAYER[i]];
+            this.outputDerivative[i] = new double[NUMBER_OF_NEURONS_IN_LAYER[i]];
 
-            this.biases[i] = NetworkTools.createRandomArray(NETWORK_LAYER_SIZES[i], 0.3, 0.7);
+            this.neuronBiases[i] = NetworkTools.createRandomArray(NUMBER_OF_NEURONS_IN_LAYER[i], 0.3, 0.7);
 
-            //Create weights array for each layer except the first
+            //Create neuronWeights array for each layer except the first
             if(i > 0){
-                weights[i] = new double[NETWORK_LAYER_SIZES[i]][NETWORK_LAYER_SIZES[i-1]];
-                weights[i] = NetworkTools.createRandomArray(NETWORK_LAYER_SIZES[i], NETWORK_LAYER_SIZES[i - 1], 0.3, 0.7);
+                neuronWeights[i] = new double[NUMBER_OF_NEURONS_IN_LAYER[i]][NUMBER_OF_NEURONS_IN_LAYER[i-1]];
+                neuronWeights[i] = NetworkTools.createRandomArray(NUMBER_OF_NEURONS_IN_LAYER[i], NUMBER_OF_NEURONS_IN_LAYER[i - 1], 0.3, 0.7);
             }
         }
 
@@ -61,85 +61,82 @@ public class Network {
     }
 
     //Feed Forward method
-    public double[] calculate(double... input){
+    public double[] feedForward(double... input){
         //Make sure that the input parameter matches the number of input neurons
-        if(input.length != this.INPUT_SIZE){ return null; }
+        if(input.length != this.INPUT_LAYER_SIZE){ return null; }
 
         //Output of first layer is the same as the input
-        this.output[0] = input;
+        this.neuronOutputs[0] = input;
 
         //for every layer, starting at 1 (because first layer is just the input
-        for(int layer = 1; layer < NETWORK_SIZE; layer++){
-            for(int neuron = 0; neuron < NETWORK_LAYER_SIZES[layer]; neuron++){
+        for(int layer = 1; layer < NUMBER_OF_LAYERS; layer++){
+            for(int neuron = 0; neuron < NUMBER_OF_NEURONS_IN_LAYER[layer]; neuron++){
 
                 //Get the sum and enter it into the activation function
                 double sum = 0;
-                for(int prevNeuron = 0; prevNeuron < NETWORK_LAYER_SIZES[layer - 1]; prevNeuron++){
-                    sum += output[layer - 1][prevNeuron] * weights[layer][neuron][prevNeuron];
+                for(int prevNeuron = 0; prevNeuron < NUMBER_OF_NEURONS_IN_LAYER[layer - 1]; prevNeuron++){
+                    sum += neuronOutputs[layer - 1][prevNeuron] * neuronWeights[layer][neuron][prevNeuron];
                 }
-                sum += biases[layer][neuron];
-                output[layer][neuron] = sigmoid(sum);
+                sum += neuronBiases[layer][neuron];
+                neuronOutputs[layer][neuron] = sigmoid(sum);
 
 
                 //Part of backpropagation
                 //TODO: Make comment
-                output_derivative[layer][neuron] = output[layer][neuron] * (1 - output[layer][neuron]);
+                outputDerivative[layer][neuron] = neuronOutputs[layer][neuron] * (1 - neuronOutputs[layer][neuron]);
 
             }
         }
-        return output[NETWORK_SIZE - 1];
+        return neuronOutputs[NUMBER_OF_LAYERS - 1];
 
     }
 
-    //TODO: Refactor eta into learningRate
     //TODO: Make comment
-    public void  train(double[] input, double[] target, double eta){
-        //Make sure input and output data are equal to amount needed
-        if(input.length != INPUT_SIZE || target.length != OUTPUT_SIZE){ return; }
-        calculate(input);
-        backPropError(target);
-        updateWeights(eta);
+    public void  train(double[] input, double[] target, double learningRate){
+        //Make sure input and neuronOutputs data are equal to amount needed
+        if(input.length != INPUT_LAYER_SIZE || target.length != OUTPUT_LAYER_SIZE){ return; }
+        feedForward(input);
+        backPropagation(target);
+        updateWeights(learningRate);
     }
 
     //Backpropagation
-    public void backPropError(double [] target){
+    public void backPropagation(double [] target){
 
         //Loop though all output neurons
-        for(int neuron = 0; neuron < NETWORK_LAYER_SIZES[NETWORK_SIZE - 1]; neuron++){
+        for(int neuron = 0; neuron < NUMBER_OF_NEURONS_IN_LAYER[NUMBER_OF_LAYERS - 1]; neuron++){
             //See graphics folder -> error_signal_equation.png
-            error_signal[NETWORK_SIZE - 1][neuron] = (output[NETWORK_SIZE - 1][neuron] - target[neuron])
-                    * output_derivative[NETWORK_SIZE - 1][neuron];
+            errorSignal[NUMBER_OF_LAYERS - 1][neuron] = (neuronOutputs[NUMBER_OF_LAYERS - 1][neuron] - target[neuron]) * outputDerivative[NUMBER_OF_LAYERS - 1][neuron];
         }
 
          //Loop through hidden layers
-        for(int layer = NETWORK_SIZE - 2; layer > 0; layer--){
+        for(int layer = NUMBER_OF_LAYERS - 2; layer > 0; layer--){
             //Loop through neurons of hidden layers
-            for(int neuron = 0; neuron < NETWORK_LAYER_SIZES[layer]; neuron++){
+            for(int neuron = 0; neuron < NUMBER_OF_NEURONS_IN_LAYER[layer]; neuron++){
                 //See second row of first equation in graphic
                 double sum = 0;
-                for(int nextNeuron = 0; nextNeuron < NETWORK_LAYER_SIZES[layer + 1]; nextNeuron++){
+                for(int nextNeuron = 0; nextNeuron < NUMBER_OF_NEURONS_IN_LAYER[layer + 1]; nextNeuron++){
                     //Increase sum by weight that connects current neuron to next neuron
-                    sum += weights[layer + 1][nextNeuron][neuron] * error_signal[layer + 1][nextNeuron];
+                    sum += neuronWeights[layer + 1][nextNeuron][neuron] * errorSignal[layer + 1][nextNeuron];
                 }
-                this.error_signal[layer][neuron] = sum * output_derivative[layer][neuron];
+                this.errorSignal[layer][neuron] = sum * outputDerivative[layer][neuron];
             }
         }
 
     }
 
-    //Update the weights
-    public void updateWeights(double eta){
-        for(int layer = 1; layer < NETWORK_SIZE; layer++){
-            for(int neuron = 0; neuron < NETWORK_LAYER_SIZES[layer]; neuron++){
-                for(int prevNeuron = 0; prevNeuron < NETWORK_LAYER_SIZES[layer - 1]; prevNeuron++){
+    //Update the neuronWeights
+    public void updateWeights(double learningRate){
+        for(int layer = 1; layer < NUMBER_OF_LAYERS; layer++){
+            for(int neuron = 0; neuron < NUMBER_OF_NEURONS_IN_LAYER[layer]; neuron++){
+                for(int prevNeuron = 0; prevNeuron < NUMBER_OF_NEURONS_IN_LAYER[layer - 1]; prevNeuron++){
                     //Weights[layer][neuron][prevNeuron]
-                    //TODO: Refactor delta to changeInWeights
-                    double delta = -eta * output[layer - 1][prevNeuron] * error_signal[layer][neuron];
-                    weights[layer][neuron][prevNeuron] += delta;
+                    double changeInWeights = -learningRate * neuronOutputs[layer - 1][prevNeuron] * errorSignal[layer][neuron];
+                    neuronWeights[layer][neuron][prevNeuron] += changeInWeights;
                 }
-                //Delta for biases
-                double delta = -eta * 1 * error_signal[layer][neuron];
-                biases[layer][neuron] += delta;
+                //Delta for neuronBiases
+                double changeInBiases = -learningRate * 1 * errorSignal[layer][neuron];
+                neuronBiases[layer][neuron] += changeInBiases;
             }
         }
     }
@@ -163,7 +160,7 @@ public class Network {
         for(int i = 0; i < 100000; i++){
             network.train(input, target, .3);
         }
-        double[] output = network.calculate(input);
+        double[] output = network.feedForward(input);
         System.out.println(Arrays.toString(output));
     }
 }
